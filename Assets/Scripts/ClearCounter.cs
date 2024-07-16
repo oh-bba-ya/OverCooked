98608542bcb219d07688454cd29506432eae77ed
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2024-07-16 16:42:41 +0900

    feat : 플레이어 Pickup 기능 구현

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,14 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour
+public class ClearCounter : MonoBehaviour,IKitchenObjectParent
 {
     [SerializeField] private KitchenObjectSO kitchenObjectso;
     [SerializeField] private Transform counterTopPoint;
 
-    public void Interact()
+
+    private KitchenObject kitchenObject;
+
+
+    public void Interact(Player player)
     {
-        Transform kitchenTransform = Instantiate(kitchenObjectso.prefab, counterTopPoint);
-        kitchenTransform.localPosition = Vector3.zero;
+        if(kitchenObject == null)
+        {
+            Transform kitchenTransform = Instantiate(kitchenObjectso.prefab, counterTopPoint);
+            kitchenTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+
+        }
+        else  // 이미 오브젝트가 존재한다면..
+        {
+            // 플레이어 줍기..
+            kitchenObject.SetKitchenObjectParent(player);
+        }
+
+
+
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,33 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectso;
 
+    private IKitchenObjectParent kitchenObjectParent;
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectso;
+    }
+
+    public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent)
+    {
+        if(this.kitchenObjectParent != null)
+        {
+            this.kitchenObjectParent.ClearKitchenObject();
+        }
+
+        this.kitchenObjectParent = kitchenObjectParent;
+
+        if(kitchenObjectParent.HasKitchenObject())
+        {
+            Debug.LogError("카운터 클래스에 이미 오브젝트가 존재합니다.");
+        }
+        kitchenObjectParent.SetKitchenObject(this);
+
+        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public IKitchenObjectParent GetKitchenObjectParent()
+    {
+        return this.kitchenObjectParent;
     }
 }
