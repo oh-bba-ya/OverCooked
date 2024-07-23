1e9978bd92cf24138c6af80e86d0a3d822f7094f
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2024-07-23 18:29:58 +0900

    Feat : Container Counter Animation 동기화
    플레이어가 카운터 인터랙션 사용시 애니메이션 동기화 기능 구현

--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -16,10 +17,21 @@ public class ContainerCounter : BaseCounter
         {
             KitchenObject.SpawnKitchenObject(kitchenObjectso, player);
 
-            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+            InteractLoginServerRpc();
         }
 
 
     }
 
+    [ServerRpc(RequireOwnership = false)]
+    private void InteractLoginServerRpc()
+    {
+        InteractLogicClientRpc();
+    }
+
+    [ClientRpc]
+    private void InteractLogicClientRpc()
+    {
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+    }
 }
