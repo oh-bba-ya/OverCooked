dc1bfcee2ecfdc968e61ce1797f69c114b01c245
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2024-07-23 20:27:09 +0900

    Feat : DeliveryCounter 삭제 동기화
    플레이어가 소유하고 있는 오브젝트 주문 카운터에서 인터랙션시 삭제 동기화

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -22,7 +22,7 @@ public class DeliveryCounter : BaseCounter
             {
                 DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
 
-                player.GetKitchenObject().DestroySelf();
+                KitchenObject.DestroyKitchenObject(player.GetKitchenObject());
             }
         }
     }
