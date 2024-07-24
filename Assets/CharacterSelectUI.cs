6b8ddcda8cd23bbc8446621fe27553e00d2fb345
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2024-07-24 23:39:48 +0900

    Feat : MainMenu & Ready 버튼
    메인 메뉴 씬으로 돌아가기 -> Network ShutDown
    
    Ready 버튼

---
+++ Assets/CharacterSelectUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectUI : MonoBehaviour
+{
+    [SerializeField] private Button mainmenuButton;
+    [SerializeField] private Button readyButton;
+
+    private void Awake()
+    {
+        mainmenuButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.Shutdown();
+            Loader.Load(Loader.Scene.MainMenuScene);
+        });
+
+        readyButton.onClick.AddListener(() =>
+        {
+            CharacterSelectReady.Instance.SetPlayerReady();
+        });
+    }
+}
